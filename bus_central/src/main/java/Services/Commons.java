b82e48df5df4ffa2158af71edcb8425e4b438ae1
Vinaya Dahal <vinayadahal@gmail.com>
2018-10-05 01:05:28 +0545

    slider code working in index page

--- bus_central/src/main/java/Services/Commons.java
+++ bus_central/src/main/java/Services/Commons.java
@@ -1,6 +1,9 @@
 package Services;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
@@ -14,6 +17,22 @@ public class Commons {
         } catch (IOException ex) {
             System.out.println("Caught IO Exception >>> " + ex);
         }
+    }
+
+    public List<String> FileList(String FilePath) {
+        List<String> files = new ArrayList<>();
+
+        File folder = new File(FilePath);
+        File[] listOfFiles = folder.listFiles();
+
+        for (File listOfFile : listOfFiles) {
+            if (listOfFile.isFile()) {
+                files.add(listOfFile.getName());
+            } else if (listOfFile.isDirectory()) {
+                System.out.println("Found Directories in: " + FilePath + listOfFile.getName());
+            }
+        }
+        return files;
     }
 
 }
